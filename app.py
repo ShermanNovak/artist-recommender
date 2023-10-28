7eb632c403eedd4325ce2083452706437ae8f777
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2023-10-28 15:50:04 +0800

    remove special characters and lemmatize

--- app.py
+++ app.py
@@ -14,10 +14,15 @@ def get_recommendations():
     if not input_artist:
         return jsonify({'error': 'Please provide an artist name in the POST request body'})
     
+    input_artist = input_artist.lower()
+
     with open('./pickle/cosine_similarity_matrix.pkl', 'rb') as file:
         cosine_sim = pickle.load(file)
 
-        recommendations = pd.DataFrame(cosine_sim.nlargest(11, input_artist)['artist_mb'])
-        recommendations = recommendations[recommendations['artist_mb'] != input_artist]
+        try: 
+            recommendations = pd.DataFrame(cosine_sim.nlargest(11, input_artist)['artist_mb'])
+            recommendations = recommendations[recommendations['artist_mb'] != input_artist]
+        except: 
+            return jsonify({'error': 'Artist not present in current database'})
     
     return jsonify({'input_artist': input_artist, 'recommended_artists': recommendations.values.flatten().tolist()})
--- createModel.py
+++ createModel.py
@@ -7,16 +7,48 @@ from sklearn.metrics.pairwise import cosine_similarity
 df = pd.read_csv('./data/artists.csv')
 
 # process and clean data
+
+# set up nltk
+# import nltk
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+def clean(tags):
+    tagList = tags.split(" ")
+    tagSet = set()
+    for tag in tagList:
+        if tag != "":
+            tagSet.add(lemmatizer.lemmatize(tag))
+    
+    return " ".join(tagSet)
+
 df2 = df[df['ambiguous_artist'] == False]
 df2 = df2[['tags_mb', 'artist_mb']]
 df2['tags_mb'] = df2['tags_mb'].str.lower()
+df2['artist_mb'] = df2['artist_mb'].str.lower()
+df2 = df2.dropna(subset=['tags_mb'])
+df2 = df2.drop_duplicates(subset=['artist_mb'])
 df2 = df2[df2['tags_mb'].apply(lambda x: isinstance(x, str))]
+
+# make each tag a single word
 df2['tags_mb'] = df2['tags_mb'].str.replace(' ', '')
 df2['tags_mb'] = df2['tags_mb'].str.replace(';', ' ')
-df2 = df2.dropna(subset=['tags_mb'])
+
+# remove special characters
+df2['tags_mb'] = df2['tags_mb'].str.replace(
+    r'[^a-z0-9\s]',
+    '',
+    regex=True,
+)
+
+# lemmatise
+df2['tags_mb'] = df2['tags_mb'].apply(clean)
 
 # limit number of rows (to avoid kernel crashing)
-df2 = df2[:1000]
+df2 = df2[:20000]
 
 # vectorise tags
 vectorizer = TfidfVectorizer()
